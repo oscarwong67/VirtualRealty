fd50681c17d9e7cda032291c0cdfd446ca1aa66a
Oscar Wong <wong.oscar67@gmail.com>
2021-11-10 14:58:21 -0700

    i think we set it up properly
    famous last words

---
+++ VirtualRealty/VirtualRealty/Top_Bar.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace VirtualRealty
+{
+    /// <summary>
+    /// Interaction logic for Top_Bar.xaml
+    /// </summary>
+    public partial class Top_Bar : UserControl
+    {
+        public Top_Bar()
+        {
+            InitializeComponent();
+        }
+
+        void GoToFavorites(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Favorites());
+        }
+    }
+}
