c4e91de3e778afc0fbb1bf13e8f2afb253d71b6b
meebee70 <christopher.kozbial@ucalgary.ca>
2021-11-24 21:20:47 -0700

    Fix Map, location still bugged

--- VirtualRealty/VirtualRealty/MapView.xaml.cs
+++ VirtualRealty/VirtualRealty/MapView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Services.Maps;
 
 namespace VirtualRealty
 {
@@ -23,6 +27,25 @@ namespace VirtualRealty
         public MapView()
         {
             InitializeComponent();
+
+            MapViewer.Loaded += MapControl_Loaded;
+        }
+
+
+
+        private async void MapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Specify a known location.
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 51.0447, Longitude = -114.0719 };
+            var cityCenter = new Geopoint(cityPosition);
+
+            // Set the map location.
+            await (sender as MapControl).TrySetViewAsync(cityCenter, 11);
+        }
+
+        public void ListView_Click(Object Sender,RoutedEventArgs args)
+        {
+            Switcher.Switch(MainWindow.LP);
         }
     }
 }
