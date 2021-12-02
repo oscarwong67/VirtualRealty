35a80685e5227f158a2603f352a006f4633e12df
meebee70 <christopher.kozbial@ucalgary.ca>
2021-12-02 14:45:37 -0700

    fix layout of favourites

--- VirtualRealty/VirtualRealty/Favorites.xaml.cs
+++ VirtualRealty/VirtualRealty/Favorites.xaml.cs
@@ -18,11 +18,51 @@ namespace VirtualRealty
     /// <summary>
     /// Interaction logic for Favorites.xaml
     /// </summary>
+
     public partial class Favorites : UserControl
     {
+
+        List<Listing> Listings;
+
         public Favorites()
         {
             InitializeComponent();
         }
+
+        public void ClearListings()
+        {
+            Listings = new List<Listing>();
+            Left.Children.Clear();
+            Centre.Children.Clear();
+            Right.Children.Clear();
+        }
+
+        public void SetListings(List<Listing> Listings)
+        {
+            int i = 0;
+            ClearListings();
+            this.Listings = Listings;
+            foreach (Listing L in Listings)
+            {
+                switch (i % 3)
+                {
+                    case 0:
+                        Left.Children.Add(L.Small);
+                        //L.Small.SetListingGrid(this.ListingPgGrid);
+                        break;
+                    case 1:
+                        Centre.Children.Add(L.Small);
+                        //L.Small.SetListingGrid(this.ListingPgGrid);
+                        break;
+                    case 2:
+                        Right.Children.Add(L.Small);
+                        //L.Small.SetListingGrid(this.ListingPgGrid);
+                        break;
+                }
+
+                i++;
+            }
+        }
+
     }
 }
