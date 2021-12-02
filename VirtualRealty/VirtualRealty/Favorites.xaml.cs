546a65b46c0f5d2a15c7b7e3e9e65ea18295a942
Oscar Wong <wong.oscar67@gmail.com>
2021-12-02 16:51:52 -0700

    faves work

--- VirtualRealty/VirtualRealty/Favorites.xaml.cs
+++ VirtualRealty/VirtualRealty/Favorites.xaml.cs
@@ -18,23 +18,27 @@ namespace VirtualRealty
     /// <summary>
     /// Interaction logic for Favorites.xaml
     /// </summary>
-
     public partial class Favorites : UserControl
     {
 
-        List<Listing> Listings;
-
+        public List<Listing> Listings;
         public Favorites()
         {
             InitializeComponent();
         }
 
+        public List<Listing> getListings()
+        {
+            return Listings;
+        }
+
+
         public void ClearListings()
         {
             Listings = new List<Listing>();
-            Left.Children.Clear();
-            Centre.Children.Clear();
-            Right.Children.Clear();
+            LeftFaves.Children.Clear();
+            CentreFaves.Children.Clear();
+            RightFaves.Children.Clear();
         }
 
         public void SetListings(List<Listing> Listings)
@@ -47,22 +51,71 @@ namespace VirtualRealty
                 switch (i % 3)
                 {
                     case 0:
-                        Left.Children.Add(L.Small);
-                        //L.Small.SetListingGrid(this.ListingPgGrid);
+                        LeftFaves.Children.Add(L.Small);
+                        L.Small.SetListingGrid(this.FavesPgGrid);
+                        //L.Small.SetListingGrid();
+                        L.Small.SetListingInd(Listings, i);
+                        L.Small.SetDisplayImage(i);
                         break;
                     case 1:
-                        Centre.Children.Add(L.Small);
-                        //L.Small.SetListingGrid(this.ListingPgGrid);
+                        CentreFaves.Children.Add(L.Small);
+                        L.Small.SetListingGrid(this.FavesPgGrid);
+                        L.Small.SetListingInd(Listings, i);
+                        L.Small.SetDisplayImage(i);
                         break;
                     case 2:
-                        Right.Children.Add(L.Small);
-                        //L.Small.SetListingGrid(this.ListingPgGrid);
+                        RightFaves.Children.Add(L.Small);
+                        L.Small.SetListingGrid(this.FavesPgGrid);
+                        L.Small.SetListingInd(Listings, i);
+                        L.Small.SetDisplayImage(i);
                         break;
                 }
-
                 i++;
             }
         }
 
+        public void MapView_Click(Object Sender, RoutedEventArgs args)
+        {
+            Switcher.Switch(MainWindow.MapViewPage);
+            List<Listing> temp = Listings;
+            foreach (Listing l in temp)
+            {
+                l.Small.SetListingGrid(MainWindow.MapViewPage.MapViewGrid);
+            }
+            ClearListings();
+            MainWindow.MapViewPage.SetListings(temp);
+        }
+
+        private void SortOrder_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Listings == null || Listings.Count == 0)
+            {
+                return;
+            }
+            List<Listing> sortedListings = new List<Listing>(Listings);
+
+            string text = ((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string;
+            if (text.Equals("Newest"))
+            {
+                sortedListings.Sort(new ListingComparer(ListingComparer.SortBy.DateListed));
+            }
+            else if (text.Equals("Oldest"))
+            {
+                sortedListings.Sort(new ListingComparer(ListingComparer.SortBy.DateListed, true /* Descending */));
+            }
+            else if (text.Equals("Price (Low to High)"))
+            {
+                sortedListings.Sort(new ListingComparer(ListingComparer.SortBy.Price, true));
+            }
+            else if (text.Equals("Price (High to Low)"))
+            {
+                sortedListings.Sort(new ListingComparer(ListingComparer.SortBy.Price));
+            }
+            else
+            {
+                sortedListings.Sort(new ListingComparer(ListingComparer.SortBy.Proximity));
+            }
+            SetListings(sortedListings);
+        }
     }
 }
